890fc63d9e4e24bf2161618bb9e14dee36d7aced
xwolf <222254017+1xwolf@users.noreply.github.com>
2026-02-25 17:31:53 +0300

    First commit

--- deploy-commands.js
+++ deploy-commands.js
@@ -102,7 +102,7 @@ const rest = new REST({ version: "10" }).setToken(config.token);
     console.log("⏳ جاري رفع الأوامر…");
 
     await rest.put(
-      Routes.applicationGuildCommands(config.clientId, config.guildId),
+      Routes.applicationCommands(config.clientId),
       { body: commands }
     );
 
